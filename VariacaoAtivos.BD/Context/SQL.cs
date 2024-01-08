84e36b25ecf85503e4c710dc0903ebdb98b1f46b
Guilherme <Guilherme@THUNDERCLOUD>
2024-01-08 01:47:25 -0300

    file system atualizacao

---
+++ VariacaoAtivos.BD/Context/SQL.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VariacaoAtivos.Domain.Entities;
+
+namespace VariacaoAtivos.BD.Context
+{
+    public class SQL : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public SQL() { }
+
+        public SQL(DbContextOptions<SQL> options) : base(options) { }
+
+        public DbSet<Ativo> AtivoConsulta { get; set; }
+    }
+}
---
+++ VariacaoAtivos.BD/Repository/BancoDados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VariacaoAtivos.BD.Context;
+using VariacaoAtivos.Domain.Entities;
+using VariacaoAtivos.Domain.Interfaces;
+
+namespace VariacaoAtivos.BD.Repository
+{
+    public class BancoDados<TEntity> : IBancoDados<TEntity> where TEntity : AtivoBase
+    {
+        protected readonly SQL sqlContext;
+
+        public BancoDados(SQL sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+
+        public void Inserir(TEntity obj)
+        {
+            this.sqlContext.Set<TEntity>().Add(obj);
+            this.sqlContext.SaveChanges();
+        }
+
+        public IList<TEntity> SelecionarTudo()
+        {
+            var result = this.sqlContext.AtivoConsulta.OrderByDescending(c => c.Dia).Take(30);
+            return (IList<TEntity>)result.ToList();
+        }
+    }
+}
